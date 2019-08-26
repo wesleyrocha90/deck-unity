c491f4e1b787fb4794e6f7872518fabe86f094d1
wesleyrocha90 <wesleyallanrocha@gmail.com>
2019-08-25 23:07:30 -0300

    Initial commit

---
+++ Assets/Editor/FallTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class FallTest 
+{
+    [Test]
+    public void CalculateTotalPoints_Test() 
+    {
+        int expectedPoints = 100;
+        int points = 100;
+
+        Assert.AreEqual(expectedPoints, points);
+    }
+}
